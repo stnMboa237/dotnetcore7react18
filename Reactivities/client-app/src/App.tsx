adf635a9a2d88596272f02d7494a90a978184c99
stnMboa237 <stngassa@live.com>
2023-01-06 13:17:19 +0100

    clientApp - installed Semantic UI and test it. End of section 3

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Button, Header, List, ListItem } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -14,16 +15,14 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
+      <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
           {activities.map((activity: any) => (
-            <li key={activity.id}>
+            <ListItem key={activity.id}>
               {activity.title}
-            </li>
+            </ListItem>
           ))}
-        </ul>
-      </header>
+        </List>
     </div>
   );
 }

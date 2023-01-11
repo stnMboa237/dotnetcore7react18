99dbbd69f4f1b2d2b58689600d6f0c54c37c31c2
stnMboa237 <stngassa@live.com>
2023-01-11 12:56:24 +0100

    BackEnd - Added MediatorPattern to link and communicate between different subProjects and Implemented CQRS Pattern with Query-Command

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -3,16 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { Button, Header, List, ListItem } from 'semantic-ui-react';
+import { LanguageServiceMode } from 'typescript';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  // Ici, on def 2 variables: activities (responsable de garder dans le state les objets take du back)
+  // et setActivities (responsable de l'update des objets Activity recupérer du back)
+  const [activities, setActivities] = useState([]); // [] initialise le state comme un array vide afin d'eviter une erreur lors du activities.map
+
+  // Ici, useEffect nous permet de faire des actions quand notre appli se lance.
+  // En l'occurence, on appelle la methode getActivities du back
   useEffect(() =>{
     axios.get('http://localhost:5000/api/activities')
     .then(response => {
       console.log(response);
       setActivities(response.data);
     })
-  }, []);
+  }, []); // [] permet d'executer la 'get' juste une fois. sinon, elle serait lancer n fois.
   return (
     <div className="App">
       <Header as='h2' icon='users' content='Reactivities'/>

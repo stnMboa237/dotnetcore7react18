3f90fb83414742c0fdb00c46eba9bc8c0992f3e2
stnMboa237 <stngassa@live.com>
2023-01-11 18:34:27 +0100

    BackEnd - End of section 4

--- Reactivities/Application/Activities/List.cs
+++ Reactivities/Application/Activities/List.cs
@@ -17,14 +17,12 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            private readonly ILogger<List> _logger;
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context)
             {
-                _logger = logger;
                 _context = context;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<List<Activity>> Handle(Query request, CancellationToken token)
             {
                 return await _context.Activities.ToListAsync();
             }

39dccc310a487bd5999cdfc665470f3b5e83bd0c
stnMboa237 <stngassa@live.com>
2023-01-18 12:07:35 +0100

    front - created the navBar component

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,25 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List, ListItem } from 'semantic-ui-react';
+import { Container, Header, List, ListItem } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
   // Ici, on def 2 variables: activities (responsable de garder dans le state les objets take du back)
   // et setActivities (responsable de l'update des objets Activity recupérer du back)
   // [] initialise le state comme un array vide afin d'eviter une erreur lors du activities.map
-  const [activities, setActivities] = useState<Activity[]>([]);  
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   // Ici, useEffect nous permet de faire des actions quand notre appli se lance.
   // En l'occurence, on appelle la methode getActivities du back
-  useEffect(() =>{
+  useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
-    .then(response => {
-      setActivities(response.data);
-    })
+      .then(response => {
+        setActivities(response.data);
+      })
   }, []); // [] permet d'executer la 'get' juste une fois. sinon, elle serait lancer n fois.
   return (
-    <div className="App">
-      <Header as='h2' icon='users' content='Reactivities'/>
+    <>
+      <NavBar />
+      <Container style={{marginTop:'7em'}}>
         <List>
           {activities.map((activity) => (
             <ListItem key={activity.id}>
@@ -27,7 +29,8 @@ function App() {
             </ListItem>
           ))}
         </List>
-    </div>
+      </Container>
+    </>
   );
 }
 

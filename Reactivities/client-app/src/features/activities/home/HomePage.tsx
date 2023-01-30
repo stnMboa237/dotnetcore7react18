f669f70696afb714cc3b88a6b8ceb229a8a8cef4
stnMboa237 <stngassa@live.com>
2023-01-30 11:11:20 +0100

    front-sect98: styled the homePage and clean old code

--- Reactivities/client-app/src/features/activities/home/HomePage.tsx
+++ Reactivities/client-app/src/features/activities/home/HomePage.tsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
 
 export default function HomePage(){
     return (
-        <Container style={{marginTop: '7em'}}>
-            <h1>Home Page</h1>
-            <h3>Go to <Link to='/activities'>Activities</Link></h3>
-        </Container>
+        <Segment inverted textAlign="center" vertical className="masthead">
+            <Container text>
+                <Header as='h1' inverted>
+                    <Image 
+                        size='massive' 
+                        src='/assets/logo.png' 
+                        alt='logo' 
+                        style={{marginBottom: 12}} 
+                    />
+                    Reactivities
+                </Header>
+                <Button as={Link} to='/activities' size='huge' inverted>Take me the activities!</Button>
+            </Container>
+        </Segment>
     )
 }

0f5d6d3cf90fa7cff007b5cdfd0e0e293a271066
stnMboa237 <stngassa@live.com>
2023-02-08 13:55:42 +0100

    back-ect137: moved secret info into the appsettings.json

--- Reactivities/API/Extensions/IdentityServiceExtensions.cs
+++ Reactivities/API/Extensions/IdentityServiceExtensions.cs
@@ -21,7 +21,7 @@ namespace API.Extensions
                 opt.Password.RequireNonAlphanumeric = false;
             }).AddEntityFrameworkStores<DataContext>();
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(opt => 
             {

05a1469d770bb315d22bfb5cd3e2fc8ef444f21f
stnMboa237 <stngassa@live.com>
2023-02-16 11:16:00 +0100

    front-sect13: managed error of RegisterForm

--- Reactivities/API/Controllers/AccountController.cs
+++ Reactivities/API/Controllers/AccountController.cs
@@ -44,12 +44,14 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName))
             {
-                return BadRequest("Username is already taken");
+                ModelState.AddModelError("Username", "Username is already taken");
+                return ValidationProblem();
             }
 
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email is already taken");
+                ModelState.AddModelError("Email", "Email is already taken");
+                return ValidationProblem();
             }
 
             var user = new AppUser

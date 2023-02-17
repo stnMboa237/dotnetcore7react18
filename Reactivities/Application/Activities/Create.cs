3a0d5861cba99e073fb65ae35919d14c9a059012
stnMboa237 <stngassa@live.com>
2023-02-17 15:20:25 +0100

    back-sect159: added an attendee when create an activity

--- Reactivities/Application/Activities/Create.cs
+++ Reactivities/Application/Activities/Create.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -17,7 +19,8 @@ namespace Application.Activities
             public Activity Activity { get; set; }
         }
 
-        public class CommandValidator: AbstractValidator<Command> {
+        public class CommandValidator : AbstractValidator<Command>
+        {
             public CommandValidator()
             {
                 RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
@@ -27,16 +30,29 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
 
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user = await _context.Users
+                    .FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUserName());
+                
+                var attendee = new ActivityAttendee {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true
+                };
+
+                request.Activity.Attendees.Add(attendee);
                 _context.Activities.Add(request.Activity);
                 var result = await _context.SaveChangesAsync() > 0;
-                if(!result) {
+                if (!result)
+                {
                     return Result<Unit>.Failure("Failed to create activity");
                 }
                 return Result<Unit>.Success(Unit.Value);
---
+++ Reactivities/Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,20 @@
+using System.Security.Claims;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor; /*httpContext contains the user Object and it Claims*/
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUserName()
+        {
+            return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+        }
+    }
+}

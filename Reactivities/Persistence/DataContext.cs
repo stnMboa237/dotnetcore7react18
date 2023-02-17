1d36dacd634901893b0318763e8eb7e5a3e1b4e2
stnMboa237 <stngassa@live.com>
2023-02-17 14:41:08 +0100

    back-sect157: added Migration ActivityAttendee

--- Reactivities/Persistence/DataContext.cs
+++ Reactivities/Persistence/DataContext.cs
@@ -9,6 +9,26 @@ namespace Persistence
         public DataContext(DbContextOptions opt) : base(opt)
         {
         }
-        public DbSet<Activity> Activities{ get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+            
+            /*begin configure MANY TO MANY relation between Activity and AppUser*/
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.AppUser)
+                .WithMany(a => a.Activities)
+                .HasForeignKey(aa => aa.AppUserId);
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.Activity)
+                .WithMany(a => a.Attendees)
+                .HasForeignKey(aa => aa.ActivityId);
+            /*End Config MANY TO MANY Activity-AppUser*/
+        }
     }
 }

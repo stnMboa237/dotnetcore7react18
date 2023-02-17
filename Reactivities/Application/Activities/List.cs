e2521af204d4b408dd7f4357352dc6fed2074ca6
stnMboa237 <stngassa@live.com>
2023-02-17 17:00:44 +0100

    back-sect14: getting related object between entities using automapper

--- Reactivities/Application/Activities/List.cs
+++ Reactivities/Application/Activities/List.cs
@@ -1,31 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Core;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken token)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken token)
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
+                var activities = await _context.Activities
+                        .Include(a => a.Attendees)
+                        .ThenInclude(u => u.AppUser)
+                        .ToListAsync();
+                var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
+                return Result<List<ActivityDto>>.Success(activitiesToReturn);
             }
         }
     }

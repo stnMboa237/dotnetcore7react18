3581744fa5e86f91b8e6f8c0175cd9b0f9cf9641
stnMboa237 <stngassa@live.com>
2023-03-04 21:47:24 +0100

    back-sect16: deleted photo

--- Reactivities/Infrastructure/Photos/PhotoAccessor.cs
+++ Reactivities/Infrastructure/Photos/PhotoAccessor.cs
@@ -37,6 +37,7 @@ namespace Infrastructure.Photos
                 if (uploadResult.Error != null) {
                     throw new Exception(uploadResult.Error.Message);
                 }
+                
                 return new PhotoUploadResult { 
                     PublicId = uploadResult.PublicId, 
                     Url = uploadResult.SecureUrl.ToString()

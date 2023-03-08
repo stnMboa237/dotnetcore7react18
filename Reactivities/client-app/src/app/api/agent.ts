e9f33ce27ac6358bd9a515dd10c6f8133dc60823
stnMboa237 <stngassa@live.com>
2023-03-08 09:30:03 +0100

    front-sect17: installed react-cropper for resizing image. upload image done.

--- Reactivities/client-app/src/app/api/agent.ts
+++ Reactivities/client-app/src/app/api/agent.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { Activity, ActivityFormValues } from '../models/activity';
-import { Profile } from '../models/profile';
+import { Photo, Profile } from '../models/profile';
 import { User, UserFormValues } from '../models/user';
 import { router } from '../router/Routes';
 import { store } from '../stores/store';
@@ -87,7 +87,14 @@ const Account = {
 }
 
 const Profiles = {
-    get: (username: string) => requests.get<Profile>(`/profiles/${username}`)
+    get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
+    uploadPhoto: (file: Blob) => {
+        let formData = new FormData ();
+        formData.append('File', file);
+        return axios.post<Photo>('photos', formData, {
+            headers: {'Content-Type': 'Content-Type'}
+        });
+    }
 }
 
 const agent = {
--- Reactivities/client-app/src/app/stores/userStore.ts
+++ Reactivities/client-app/src/app/stores/userStore.ts
@@ -53,4 +53,10 @@ export default class UserStore {
             console.log(error);
         }
     }
+
+    setImage = (image: string) => {
+        if(this.user){
+            this.user.image = image;
+        }
+    }
 }

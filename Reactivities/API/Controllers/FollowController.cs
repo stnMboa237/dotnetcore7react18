849352a90b18f588f4f32bc293878b83e208462b
stnMboa237 <stngassa@live.com>
2023-03-23 14:02:45 +0100

    back-sect20: added following info to activities list/details

--- Reactivities/API/Controllers/FollowController.cs
+++ Reactivities/API/Controllers/FollowController.cs
@@ -9,5 +9,13 @@ namespace API.Controllers
         public async Task<IActionResult> Follow(string username) {
             return HandleResult(await Mediator.Send(new FollowToggle.Command {TargetUsername = username}));
         }
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> getFollowings(string username, string predicate) {
+            return HandleResult(await Mediator.Send(new List.Query {
+                Username = username,
+                Predicate = predicate
+            }));
+        }
     }
 }

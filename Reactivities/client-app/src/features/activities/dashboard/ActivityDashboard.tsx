4b1bf3ec67f5afd92e2b33174a636d5baeb40237
stnMboa237 <stngassa@live.com>
2023-03-27 12:23:01 +0200

    front-sect2: added pagination example on the ActivityDasboard component

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,7 +1,8 @@
 import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
-import { Grid } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import { Button, Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/loadingComponent';
+import { PagingParams } from '../../../app/models/pagination';
 import { useStore } from '../../../app/stores/store';
 import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
@@ -10,7 +11,14 @@ import ActivityList from './ActivityList';
 export default observer(function ActivityDashboard() {
 
     const { activityStore } = useStore();
-    const { loadActivities, activityRegistry } = activityStore;
+    const { loadActivities, activityRegistry, setPagingParams, pagination } = activityStore;
+    const [ loadingNext, setLoadingNext ]= useState(false);
+
+    function handleGetNext() {
+        setLoadingNext(true);
+        setPagingParams(new PagingParams(pagination!.currentPage + 1));
+        loadActivities().then(() => setLoadingNext(false));
+    }
 
     // Ici, useEffect nous permet de faire des actions quand notre appli se lance.
     // En l'occurence, on appelle la methode getActivities du back
@@ -21,12 +29,20 @@ export default observer(function ActivityDashboard() {
         }
     }, [activityRegistry.size, loadActivities]); // [] permet d'executer la 'get' juste une fois. sinon, elle serait lancer n fois.
 
-    if (activityStore.loadingInitial) return (<LoadingComponent content='Loading activities...' />)
+    if (activityStore.loadingInitial && !loadingNext) return (<LoadingComponent content='Loading activities...' />)
 
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList />
+                <Button 
+                    floated="right" 
+                    content="More..." 
+                    positive
+                    onClick={handleGetNext}
+                    loading={loadingNext} 
+                    disabled={pagination?.totalPages === pagination?.currentPage}
+                />
             </Grid.Column>
             <Grid.Column width='6'>
                 <ActivityFilters />

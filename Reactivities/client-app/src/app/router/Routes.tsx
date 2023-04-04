e4b1f3f1d193f7125d060bd5a7841e148ea9911d
stnMboa237 <stngassa@live.com>
2023-04-04 10:04:15 +0200

    front-sect22: adding private route

--- Reactivities/client-app/src/app/router/Routes.tsx
+++ Reactivities/client-app/src/app/router/Routes.tsx
@@ -8,19 +8,25 @@ import TestErrors from "../../features/errors/TestError";
 import ProfilePage from "../../features/profiles/ProfilePage";
 import LoginForm from "../../features/users/LoginForm";
 import App from "../layout/App";
+import RequiredAuth from "./RequiredAuth";
 
 export const routes: RouteObject[] = [
     {
         path: '/',
         element: <App />,
-        children: [
-            {path: 'activities', element: <ActivityDashboard />},
-            {path: 'activities/:id', element: <ActivityDetails />},
-            {path: 'createActivity', element: <ActivityForm key='create'/>}, /*unsing Key inside the same component allows react to reset the state when the component Key changed*/
-            {path: 'manage/:id', element: <ActivityForm key='manage'/>},
-            {path: 'profiles/:username', element: <ProfilePage/>},
-            {path: 'login', element: <LoginForm/>},
-            {path: 'errors', element: <TestErrors />},
+        children: [ /* element est appelé est parent de toutes nos routes. ainsi, 
+        si l'user tape sur la barre d'adresse une route, 
+        alors il y aura toujours un check pour verif que l'user est authentifié, 
+        sinon, il sera renvoyé vers la page d'accueil */
+            {element: <RequiredAuth/ >, children: [
+                {path: 'activities', element: <ActivityDashboard />},
+                {path: 'activities/:id', element: <ActivityDetails />},
+                {path: 'createActivity', element: <ActivityForm key='create'/>}, /*unsing Key inside the same component allows react to reset the state when the component Key changed*/
+                {path: 'manage/:id', element: <ActivityForm key='manage'/>},
+                {path: 'profiles/:username', element: <ProfilePage/>},
+                {path: 'login', element: <LoginForm/>},
+                {path: 'errors', element: <TestErrors />}
+            ]},
             {path: 'not-found', element: <NotFound />},
             {path: 'server-error', element: <ServerError />},
             {path: '*', element: <Navigate replace to='/not-found' />},
